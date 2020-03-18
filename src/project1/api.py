fd5da3dee11b2045210ba497e1debd71bdb61696
Lesley20 <jinmei.li@baruchmail.cuny.edu>
2020-03-18 00:14:28 -0400

    modified

--- src/project1/api.py
+++ src/project1/api.py
@@ -1,39 +1,35 @@
 import json
-
 from sodapy import Socrata
-
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
 def create_and_update_index(index_name, doc_type):
 	es = Elasticsearch(timeout=10)
-
 	try:
-		es.indices.create(index=index_name)
+		es.indices.create(index=index_name, doc_type=doc_type)
 	except Exception:
 		pass
-
-	"""
+	
 	es.indices.put_mapping(
 		index=index_name,
 		doc_type=doc_type,
 		body={
 			doc_type:{
-				"properties":{"location": {"type": "geo_point"} ,
+				"properties":{"issue_date": { "type": "date"}, 
 				}
 			}
 		}
 	)
-	"""
+	
 
 	return es
 
 
 def load_ES(results, es):
+	print(results)
 	for result in results:
 		res = es.index(index="violationparking-index", doc_type="violations", body = result,)
 		print(res['result'])
-
 
 
 def get_data(APP_KEY, page_size, num_pages, output):
@@ -41,15 +37,20 @@ def get_data(APP_KEY, page_size, num_pages, output):
 		client = Socrata('data.cityofnewyork.us', APP_KEY)
 		es = create_and_update_index('violationparking-index', 'violations')
 
-		if num_pages !='':
+
+		if num_pages == '':
 			num_row = client.get('nc67-uf89', select='COUNT(*)') #count num of row in dataset
+
+			print("SOMETHING")
+
 			total = int(num_row[0]['COUNT'])
 			num_pages = (total / page_size) #get the num of pages 
 			return num_pages
 
+
 		else: 
-			es = Elasticsearch()
-			
+			#es = Elasticsearch()
+
 			for i in range (num_pages):
 				data = client.get('nc67-uf89', limit=page_size, offset=i*page_size)
 
@@ -57,12 +58,15 @@ def get_data(APP_KEY, page_size, num_pages, output):
 					with open(output, 'a') as fout:
 						for result in data:
 							fout.write(json.dumps(result) + '\n')
+							
+							print("HERE")
 
-							load_ES(result,es)
+							load_ES(result, es)
 
 				else:
 					for result in data:
 						print(result + '\n')
+
 
 	except Exception as e:
 		print(f'Something went wrong {e}')
